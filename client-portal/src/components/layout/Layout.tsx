ab53590557984d0a8035251f5eaa3097025f9574
Dimitur Kazakov <mkazakov200420@gmail.com>
2024-05-14 22:42:00 +0300

    [CLIENT-PORTAL] setup

---
+++ client-portal/src/components/layout/Layout.tsx
@@ -0,0 +1,17 @@
+import { Box } from '@mui/material';
+import { Navigation } from './Navigation';
+
+type LayoutProps = {
+  children: JSX.Element;
+};
+export const Layout = (props: LayoutProps) => {
+  const { children } = props;
+
+  return (
+    <Box className="tw-bg-slate-300 tw-h-screen">
+      <Navigation />
+
+      <Box className="tw-bg-slate-300 tw-h-100">{children}</Box>
+    </Box>
+  );
+};
---
+++ client-portal/src/components/layout/Navigation.tsx
@@ -0,0 +1,127 @@
+import MenuIcon from '@mui/icons-material/Menu';
+import {
+  AppBar,
+  Box,
+  Button,
+  Container,
+  IconButton,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Typography,
+} from '@mui/material';
+import { useRouter } from 'next/router';
+import { useState } from 'react';
+
+const pages = [
+  { name: 'Recipes', path: '/recipe' },
+  { name: 'Menus', path: '/menu' },
+  { name: 'Daily Programs', path: '/dailyProgram' },
+];
+
+export const Navigation = () => {
+  const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
+  const router = useRouter();
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  return (
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            KITCHEN MASTER
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={(e) => setAnchorElNav(e.currentTarget)}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem
+                  key={page.name}
+                  onClick={() => {
+                    router.push(page.path);
+                    handleCloseNavMenu();
+                  }}
+                >
+                  <Typography textAlign="center">{page.name}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            KITCHEN MASTER
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <Button
+                key={page.name}
+                onClick={() => {
+                  router.push(page.path);
+                  handleCloseNavMenu();
+                }}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                {page.name}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
